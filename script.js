398391ab76ffde93720f469e118fc9b90f59abf9
mirabellej <mirabellejones@gmail.com>
2020-07-10 18:28:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ const video = document.getElementById("video");
 const expression_threshold = 0.9; // threshold value over which we deduce emotion to be current emotion
 const expression_interval = 1000; // take reading of expression ever n milliseconds
 
-// to do break this down into switch statement to play specific sound sample from bank for each expression
-function playSound() {
-  var audio = new Audio("/beep.mp3");
-  audio.play();
-}
+// timers for speech
+var pause_length = 4000; // max pause between phrases
+var next_utterance = Date.now() + pause_length;
+
+setTimeout(pause, pause_length);
+
+// timers for experiment
+var experiment_length = 10000; // 600000 ms = 10 minutes
+var expected = Date.now() + experiment_length;
+var experiment_over = false;
+
+checkTTS();
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models/"),
@@ -15,17 +22,14 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri("/models/"),
 ]).then(startVideo());
 
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    (stream) => (video.srcObject = stream),
-    (err) => console.error(err)
-  );
-}
-
 video.addEventListener("play", () => {
   processVideo();
 });
+
+setTimeout(step, experiment_length);
+setTimeout(pause, pause_length);
+
+/* Computer Vision and Video Functions*/
 
 function processVideo() {
   const canvas = faceapi.createCanvasFromMedia(video);
@@ -50,11 +54,20 @@ function processVideo() {
         current_expression = key;
         //console.log(key);
         if (current_expression === "happy") {
-          playSound();
+          //playSound();
+          playGreeting();
         }
       }
     }
   }, expression_interval);
+}
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
 function determineExpression() {
@@ -65,4 +78,60 @@ function determineExpression() {
       console.log(`${key}: ${value}`);
     }
   }
+}
+
+/* Text To Speech Functions */
+
+function checkTTS() {
+  if ("speechSynthesis" in window) {
+    // Speech Synthesis supported
+  } else {
+    // Speech Synthesis Not Supported
+    alert(
+      "Sorry, your browser doesn't support text to speech. Please exit the experiment."
+    );
+  }
+}
+
+function playGreeting() {
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[0];
+  //msg.volume = 1; // From 0 to 1
+  //msg.rate = 1; // From 0.1 to 10
+  //msg.pitch = 2; // From 0 to 2
+  msg.text = "I am reading a story.";
+  speechSynthesis.speak(msg);
+}
+
+// to do break this down into switch statement to play specific sound sample from bank for each expression
+function playSound() {
+  var audio = new Audio("/beep.mp3");
+  audio.play();
+}
+
+/* Timing functions */
+
+function step() {
+  var dt = Date.now() - expected; // the drift (positive for overshooting)
+  if (dt > experiment_length) {
+    timer_elapsed = true; // end experiment if overdue - this should never happen
+  }
+  timer_elapsed = true;
+  alert("time's up!");
+  // TO DO: end experiment - go to survey
+  expected += experiment_length;
+  setTimeout(step, Math.max(0, experiment_length - dt)); // take into account drift
+}
+
+function pause() {
+  var dt = Date.now() - next_utterance; // the drift (positive for overshooting)
+  if (dt > pause_length) {
+    // something really bad happened. Maybe the browser (tab) was inactive?
+    // possibly special handling to avoid futile "catch up" run
+  }
+  alert("pause!");
+
+  next_utterance += pause_length;
+  setTimeout(pause, Math.max(0, pause_length - dt)); // take into account drift
 }
